45ec368776ccb250bfdfcd352c4df83022c53ae3
Arvind <arvindchoudhary054@gmail.com>
2026-03-04 22:56:25 +0530

    TanStack Query Setup

---
+++ components/providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+import { SessionProvider } from "@/components/sessionProvider";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <SessionProvider>{children}</SessionProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
